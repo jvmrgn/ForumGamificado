cf34377ccafc702221c6ce130d492be20ff6edc6
JvGm45 <joaoviniciusjv45.gm@gmail.com>
2024-04-29 17:51:37 -0300

    News

---
+++ src/Components/MainPage/MainPage.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import style from "./MainPage.module.css";
+
+function MainPage() {
+  return (
+    <div className={style.content}>
+      <h1 className={style.title}>Seja bem-vindo!</h1>
+      <p>
+        Espero que você se divirta utilizando nossa rede social! Para encontrar
+        os posts
+      </p>
+      <a className={style.cliqueAqui} href="http://localhost:5174/posts">
+        Clique aqui!
+      </a>
+    </div>
+  );
+}
+
+export default MainPage;
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import AppBar from "../Components/AppBar/AppBar";
+import MainPage from "../Components/MainPage/MainPage";
 
 function Home() {
   return (
     <div>
       <AppBar />
+      <MainPage />
     </div>
   );
 }
